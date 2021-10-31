d68e338a9a60c1e7c64b73bace250d2ede0607fc
Karan Odedra <karanodedra81@gmail.com>
2021-10-31 12:56:21 +0530

    added router links for navigation

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,12 +3,23 @@
   <header id="header" class="bg-gray-700">
     <nav class="container mx-auto flex justify-start items-center py-5 px-4">
       <!-- App Name -->
-      <a class="text-white font-bold uppercase text-2xl mr-4" href="#">Music</a>
+      <router-link
+        class="text-white font-bold uppercase text-2xl mr-4"
+        :to="{ name: 'home' }"
+        exact-active-class="no-active"
+      >
+        Music
+      </router-link>
 
       <div class="flex flex-grow items-center">
         <!-- Primary Navigation -->
         <ul class="flex flex-row mt-1">
           <!-- Navigation Links -->
+          <li>
+            <router-link class="px-2 text-white" :to="{ name: 'about' }"
+              >About</router-link
+            >
+          </li>
           <li v-if="!userLoggedIn">
             <a
               class="px-2 text-white"
@@ -20,7 +31,9 @@
           </li>
           <template v-else>
             <li>
-              <a class="px-2 text-white" href="#">Manage</a>
+              <router-link class="px-2 text-white" :to="{ name: 'manage' }"
+                >Manage</router-link
+              >
             </li>
             <li>
               <a class="px-2 text-white" href="#" @click.prevent="signout">
@@ -35,7 +48,7 @@
 </template>
 
 <script>
-import { mapMutations, mapState, mapActions } from "vuex";
+import { mapMutations, mapState } from "vuex";
 
 export default {
   name: "AppHeader",
@@ -48,10 +61,15 @@ export default {
     //   this.$store.commit("toggleAuthModal");
     // },
 
-    ...mapActions(["signout"]),
-    // signout() {
-    //   this.$store.dispatch("signout");
-    // },
+    // ...mapActions(["signout"]),
+    signout() {
+      this.$store.dispatch("signout");
+
+      if (this.$router.name === "manage") {
+        // redirect to Home page, only if we are in manage page, else stay on the same page
+        this.$router.push({ name: "home" });
+      }
+    },
   },
 };
 </script>
