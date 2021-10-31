5894f4f172a4b34d3e235882b7a12409dde0d8fd
Karan Odedra <karanodedra81@gmail.com>
2021-10-31 18:26:13 +0530

    moved logic of routing from component to global

--- src/views/Manage.vue
+++ src/views/Manage.vue
@@ -304,16 +304,7 @@
 </template>
 
 <script>
-import store from "@/store";
-
 export default {
   name: "manage",
-  beforeRouterEnter(to, from, next) {
-    if (store.state.userLoggedIn) {
-      next();
-    } else {
-      next({ name: "home" });
-    }
-  },
 };
 </script>
