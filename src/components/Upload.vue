a535cc8c22e26fb151d010b784db7a15211ce0a7
Karan Odedra <karanodedra81@gmail.com>
2022-02-06 11:18:27 +0530

    to store the file details in firebase storage and an input type to upload files

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -34,6 +34,7 @@
       >
         <h5>Drop your files here</h5>
       </div>
+      <input type="file" multiple @change="upload($event)" />
       <hr class="my-6" />
       <!-- Progess Bars -->
       <div class="mb-4" v-for="upload in uploads" :key="upload.name">
@@ -56,7 +57,7 @@
 
 <script>
 import { defineComponent } from "vue";
-import { storage } from "../includes/firebase";
+import { storage, auth, songsCollection } from "../includes/firebase";
 
 export default defineComponent({
   name: "Upload",
@@ -69,7 +70,11 @@ export default defineComponent({
   methods: {
     upload($event) {
       this.is_dragover = false;
-      const files = [...$event.dataTransfer.files];
+
+      const files = $event.dataTransfer
+        ? [...$event.dataTransfer.files]
+        : [...$event.target.files];
+
       files.forEach((file) => {
         if (file.type !== "audio/mpeg") {
           return;
@@ -101,7 +106,19 @@ export default defineComponent({
             this.uploads[uploadIndex].text_class = "text-red-400";
             console.log(error);
           },
-          () => {
+          async () => {
+            const song = {
+              uid: auth.currentUser.uid,
+              display_name: auth.currentUser.displayName,
+              original_name: task.snapshot.ref.name,
+              modified_name: task.snapshot.ref.name,
+              genre: "",
+              comment_count: 0,
+            };
+
+            song.url = await task.snapshot.ref.getDownloadURL();
+            await songsCollection.add(song);
+
             this.uploads[uploadIndex].variance = "bg-green-400";
             this.uploads[uploadIndex].icon = "fas fa-check";
             this.uploads[uploadIndex].text_class = "text-green-400";
