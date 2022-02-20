56973d3af69c8281da2b7f503a3e124514c77276
Karan Odedra <karanodedra81@gmail.com>
2022-02-20 11:25:00 +0530

    to sort the comments

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -79,6 +79,7 @@
         </vee-form>
         <!-- Sort Comments -->
         <select
+          v-model="sort"
           class="
             block
             mt-4
@@ -102,7 +103,7 @@
   <ul class="container mx-auto">
     <li
       class="p-6 bg-gray-50 border border-gray-200"
-      v-for="comment in comments"
+      v-for="comment in sortedComments"
       :key="comment.docID"
     >
       <!-- Comment Author -->
@@ -136,10 +137,20 @@ export default defineComponent({
       comment_alert_variant: "bg-blue-500",
       comment_alert_message: "Please wait! Your comment is being submitted",
       comments: [],
+      sort: "1",
     };
   },
   computed: {
     ...mapState(["userLoggedIn"]),
+    sortedComments() {
+      return this.comments.slice().sort((a, b) => {
+        // slice - to return new array, can't modify data property directly
+        if (this.sort === "1") {
+          return new Date(b.datePosted) - new Date(a.datePosted);
+        }
+        return new Date(a.datePosted) - new Date(b.datePosted);
+      });
+    },
   },
   async created() {
     const docSnapshot = await songsCollection.doc(this.$route.params.id).get();
