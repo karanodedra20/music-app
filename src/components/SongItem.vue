2b168e15750cf32b10f135481962ae284a26a65e
Karan Odedra <karanodedra81@gmail.com>
2022-02-20 09:02:58 +0530

    songs component

---
+++ src/components/SongItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <li
+    class="
+      flex
+      justify-between
+      items-center
+      p-3
+      pl-6
+      cursor-pointer
+      transition
+      duration-300
+      hover:bg-gray-50
+    "
+  >
+    <div>
+      <a href="#" class="font-bold block text-gray-600">
+        {{ song.modified_name }}
+      </a>
+      <span class="text-gray-500 text-sm">{{ song.display_Name }}</span>
+    </div>
+
+    <div class="text-gray-600 text-lg">
+      <span class="comments">
+        <i class="fa fa-comments text-gray-600"></i>
+        {{ song.comment_count }}
+      </span>
+    </div>
+  </li>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "SongItem",
+  props: ["song"],
+});
+</script>
+
+<style lang="scss" scoped></style>
